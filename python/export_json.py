5ff456175985d1c897d351166269f0b9def7ec0f
Robert Rajakone <robie2011@users.noreply.github.com>
2020-07-17 11:04:07 +0200

    Create export_json.py

---
+++ python/export_json.py
@@ -0,0 +1,7 @@
+import json 
+
+def write_column_file_json(directory, db, table, result):
+    parent_dir = f'{directory}/{db}'
+    os.makedirs(parent_dir, exist_ok=True)
+    with open(f'{parent_dir}/{table}.json', 'w') as fd:
+        json.dump(result, fd, indent=4)
